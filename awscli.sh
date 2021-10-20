c79c3b5a299204e87131c3be16abf47c8536b94c
varun656 <var656@gmail.com>
2021-10-20 17:33:54 +0100

    ssm cli

--- awscli.sh
+++ awscli.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 aws --version
+aws ssm put-parameter --name "/codebuild/securityCheck" --type "String" --value "testvalue" --overwrite
 aws s3 cp package.json s3://lambdacust007/package.json
