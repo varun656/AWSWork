2336d479fcde82f178973349a26cdad723044b25
varun656 <65955875+varun656@users.noreply.github.com>
2025-12-23 15:15:45 +0000

    Update Kernel.php

--- portal/app/Console/Kernel.php
+++ portal/app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
     }
 
     /**
-     * Register the Closure based commands for the application 2.
+     * Register the Closure based commands for the application.
      *
      * @return void
      */
